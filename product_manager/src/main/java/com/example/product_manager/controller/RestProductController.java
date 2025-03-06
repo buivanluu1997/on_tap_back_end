0000fabde798a7f41fd7dfc1206c481e90877369
unknown <buivanluu1997@gmail.com>
2025-03-06 15:18:53 +0700

    first commit

--- product_manager/src/main/java/com/example/product_manager/controller/RestProductController.java
+++ product_manager/src/main/java/com/example/product_manager/controller/RestProductController.java
@@ -1,4 +1,72 @@
 package com.example.product_manager.controller;
 
+import com.example.product_manager.model.Product;
+import com.example.product_manager.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/products")
 public class RestProductController {
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping("/")
+    public ResponseEntity<Page<Product>> searchNameAndCategoryId(@RequestParam(required = false, defaultValue = "") String name,
+                                                                 @RequestParam(required = false) Integer categoryId,
+                                                                 @RequestParam(required = false, defaultValue = "0") int page,
+                                                                 @RequestParam(required = false, defaultValue = "4") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> searchByNameAndCategoryIdContaining = productService.searchNameAndCategoryId(name, categoryId, pageable);
+        if (searchByNameAndCategoryIdContaining == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(searchByNameAndCategoryIdContaining, HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> saveProduct(@RequestBody Product product) {
+        try {
+           productService.addProduct(product);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Lỗi khi thêm sản phẩm: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer id) {
+        Product product = productService.findByIdProduct(id);
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable Integer id) {
+        Product product = productService.findByIdProduct(id);
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable Integer id, @RequestBody Product product) {
+        Product product1 = productService.findByIdProduct(id);
+        if (product1 == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        product.setId(id);
+        productService.updateProduct(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
 }
